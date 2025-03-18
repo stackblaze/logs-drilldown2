8499f16d7b6a1b7fc46e8857bfeb0c66d35c32c5
Sven Grossmann <Svennergr@gmail.com>
2025-03-18 08:21:20 +0100

    Investigations: Add `fieldConfig` to investigations context (#1124)
    * Investigations: Add `fieldConfig` to investigations context
    
    * feat(investigations): dedupe fieldconfig
    
    * feat(investigations): fix conditional

--- src/Components/ServiceScene/Breakdowns/AddToInvestigationButton.tsx
+++ src/Components/ServiceScene/Breakdowns/AddToInvestigationButton.tsx
@@ -1,6 +1,13 @@
-import { DataFrame, TimeRange } from '@grafana/data';
+import { DataFrame, FieldConfig, FieldConfigSource, TimeRange } from '@grafana/data';
 import { usePluginLinks } from '@grafana/runtime';
-import { SceneComponentProps, sceneGraph, SceneObjectBase, SceneObjectState, SceneQueryRunner } from '@grafana/scenes';
+import {
+  SceneComponentProps,
+  sceneGraph,
+  SceneObjectBase,
+  SceneObjectState,
+  SceneQueryRunner,
+  VizPanel,
+} from '@grafana/scenes';
 import { DataSourceRef } from '@grafana/schema';
 import { IconButton } from '@grafana/ui';
 import React from 'react';
@@ -32,6 +39,7 @@ type ExtensionContext = {
   logoPath: string;
   note?: string;
   drillDownLabel?: string;
+  fieldConfig?: FieldConfigSource;
 };
 
 export class AddToInvestigationButton extends SceneObjectBase<AddToInvestigationButtonState> {
@@ -50,6 +58,7 @@ export class AddToInvestigationButton extends SceneObjectBase<AddToInvestigation
         if (!this.state.queries.length) {
           this.getQueries();
         }
+
         if (!this.state.context && this.state.queries.length) {
           this.getContext();
         }
@@ -60,6 +69,7 @@ export class AddToInvestigationButton extends SceneObjectBase<AddToInvestigation
   private getQueries = () => {
     const data = sceneGraph.getData(this);
     const queryRunner = findObjectOfType(data, (o) => o instanceof SceneQueryRunner, SceneQueryRunner);
+
     if (queryRunner) {
       const filter = this.state.frame ? getFilter(this.state.frame) : null;
       const queries = queryRunner.state.queries.map((q) => ({
@@ -68,13 +78,55 @@ export class AddToInvestigationButton extends SceneObjectBase<AddToInvestigation
         legendFormat: filter?.name ? `{{ ${filter.name} }}` : sceneGraph.interpolate(queryRunner, q.legendFormat),
         datasource: q.datasource ?? undefined,
       }));
+
       if (JSON.stringify(queries) !== JSON.stringify(this.state.queries)) {
         this.setState({ queries });
       }
     }
   };
 
+  private getFieldConfig = () => {
+    const panel = findObjectOfType(this, (o) => o instanceof VizPanel, VizPanel);
+    const data = sceneGraph.getData(this);
+    const frames = data?.state.data?.series;
+    let fieldConfig = panel?.state.fieldConfig;
+    if (fieldConfig && frames?.length) {
+      for (const frame of frames) {
+        for (const field of frame.fields) {
+          const configKeys = Object.keys(field.config);
+          const properties = configKeys.map((key) => ({
+            id: key,
+            value: field.config[key as keyof FieldConfig],
+          }));
+
+          // check if the override already exists
+          const existingOverride = fieldConfig.overrides.find(
+            (o) =>
+              o.matcher.options === (field.config.displayNameFromDS ?? field.config.displayName ?? field.name) &&
+              o.matcher.id === 'byName'
+          );
+          if (!existingOverride) {
+            // add as first override
+            fieldConfig.overrides.unshift({
+              matcher: {
+                id: 'byName',
+                options: field.config.displayNameFromDS ?? field.config.displayName ?? field.name,
+              },
+              properties,
+            });
+          }
+
+          if (existingOverride && JSON.stringify(existingOverride.properties) !== JSON.stringify(properties)) {
+            existingOverride.properties = properties;
+          }
+        }
+      }
+    }
+    return fieldConfig;
+  };
+
   private getContext = () => {
+    const fieldConfig = this.getFieldConfig();
     const { queries, ds, labelName, fieldName, type } = this.state;
     const timeRange = sceneGraph.getTimeRange(this);
 
@@ -92,6 +144,7 @@ export class AddToInvestigationButton extends SceneObjectBase<AddToInvestigation
       title: `${labelName}${fieldName ? ` > ${fieldName}` : ''}`,
       logoPath: LokiLogo,
       drillDownLabel: fieldName,
+      fieldConfig: fieldConfig,
     };
     if (JSON.stringify(ctx) !== JSON.stringify(this.state.context)) {
       this.setState({ context: ctx });
