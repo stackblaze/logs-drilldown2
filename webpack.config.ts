2121df3946454fc1416b0ff041c8207d51436d44
Liza Detrick <114438185+L2D2Grafana@users.noreply.github.com>
2025-05-07 14:59:39 -0700

    chore(bundle-stats): swap bundlewatch for cp gha bundle-stats (#1252)

--- webpack.config.ts
+++ webpack.config.ts
@@ -3,7 +3,6 @@ import LiveReloadPlugin from 'webpack-livereload-plugin';
 import { merge } from 'webpack-merge';
 
 import grafanaConfig from './.config/webpack/webpack.config';
-// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 const config = async (env: any): Promise<Configuration> => {
   const baseConfig = await grafanaConfig(env);
@@ -13,7 +12,6 @@ const config = async (env: any): Promise<Configuration> => {
       asyncWebAssembly: true,
     },
     plugins: [
-      // new BundleAnalyzerPlugin()
       new LiveReloadPlugin({
         appendScriptTag: true,
         delay: 1000,
