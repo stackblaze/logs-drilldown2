7eb6b3e8c78df70aba428f7daddd739ca2713846
Liza Detrick <114438185+L2D2Grafana@users.noreply.github.com>
2025-07-24 16:47:43 -0700

    omit: hide certain commit msgs (#1442)

---
+++ .versionrc.js
@@ -0,0 +1,9 @@
+// Commit types omitted from changelogs
+module.exports = {
+  types: [
+    { type: 'omit', section: 'Omitted', hidden: true },
+    { type: 'internal', section: 'Internal', hidden: true },
+    { type: 'chore', section: 'Chores', hidden: true },
+    { type: 'skip', section: 'Skipped', hidden: true },
+  ],
+};
--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,7 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
+  rules: {
+    // Commit types omitted from changelogs
+    'type-enum': [2, 'always', ['omit', 'internal', 'chore', 'skip']],
+  },
 };
