025ed25629d3b710b589ed39c5b1124a2bb48c44
Liza Detrick <114438185+L2D2Grafana@users.noreply.github.com>
2025-09-11 13:34:33 -0700

    chore(faro): faro log successful plugin load (#1529)

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -1,11 +1,17 @@
 import React, { lazy } from 'react';
 
 import { AppRootProps } from '@grafana/data';
-const LogExplorationView = lazy(() => import('./LogExplorationPage'));
 
+import { logger } from 'services/logger';
+
+const LogExplorationView = lazy(() => import('./LogExplorationPage'));
 const PluginPropsContext = React.createContext<AppRootProps | null>(null);
 
 class App extends React.PureComponent<AppRootProps> {
+  componentDidMount() {
+    // Log plugin loading success for SLO monitoring
+    logger.info('Plugin loaded successfully');
+  }
   render() {
     return (
       <PluginPropsContext.Provider value={this.props}>
